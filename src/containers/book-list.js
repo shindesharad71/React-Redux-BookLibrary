1ea82db5796b48acc77d0d9d1cab0afa1bbaca3f
Sharad Shinde <shindesharad71@gmail.com>
2017-08-26 19:46:02 +0530

    code init

---
+++ src/containers/book-list.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+export default class BookList extends Component {
+    
+    renderList() {
+        return this.props.books.map((book) => {
+            <li className="list-group-item" key={book.title}>{book.title}</li>
+        });
+    }
+
+    render() {
+        return(
+            <ul className="list-group col-sm-4">
+                { this.renderList }
+            </ul>
+        );
+    }
+}
---
+++ src/reducers/reducer_book.js
@@ -0,0 +1,10 @@
+export default function() {
+    return [
+        {title: 'My Book 1'},
+        {title: 'Onother Book'},
+        {title: 'Book 3'},
+        {title: 'Book4'},
+        {title: 'Book Five'},
+        {title: 'Book Six'}
+    ];
+}
